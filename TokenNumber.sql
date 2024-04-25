5e7693d83efef7629021ad113d7a858b305e47d5
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2024-04-25 05:04:44 +0300

    Add files via upload

---
+++ TokenNumber.sql
@@ -0,0 +1,45 @@
+-- Solution 1:
+select token_num
+from (select distinct * from tokens) t
+group by token_num
+having count(token_num) = 1
+order by token_num 
+limit 1
+
+
+
+-- Solution 2:
+select min(token_num) as token_num
+from (
+	select token_num
+	from (
+		select token_num, customer
+		from tokens group by token_num, customer
+		order by token_num) x 
+	group by token_num	
+	having count(1) = 1) y;
+
+
+
+-- Solution 3:
+select token_num
+from (
+	select token_num, customer
+	from tokens group by token_num, customer
+	order by token_num) x 
+group by token_num	
+having count(1) = 1
+order by token_num 
+limit 1;
+
+
+-- Solution 4:
+WITH cte AS (
+    SELECT token_num, COUNT(DISTINCT(customer)) AS Count
+    FROM tokens
+    GROUP BY token_num
+)
+
+SELECT MIN(token_num) AS token_num
+FROM cte
+WHERE Count = 1;
---
+++ TokenNumberDataset.sql
@@ -0,0 +1,21 @@
+drop table if exists tokens;
+create table tokens
+(
+	token_num	int,
+	customer	varchar(20)
+);
+insert into tokens values(1, 'Maryam');
+insert into tokens values(2, 'Rocky');
+insert into tokens values(3, 'John');
+insert into tokens values(3, 'John');
+insert into tokens values(2, 'Arya');
+insert into tokens values(1, 'Pascal');
+insert into tokens values(9, 'Kate');
+insert into tokens values(9, 'Ibrahim');
+insert into tokens values(8, 'Lilly');
+insert into tokens values(8, 'Lilly');
+insert into tokens values(5, 'Shane');
+
+select * from tokens;
+
+
